4b0795572c8f2f701719f049820bb65097c07f99
Strievskiy <alkstr98@gmail.com>
2019-06-15 11:07:13 +0300

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Test {
+    public static void main(String[] args) {
+        Lexer lexer = new Lexer("src/Test.txt");
+        List<Token> tokens = lexer.do_lex();
+
+        for (Token t : tokens){
+            System.out.println(t.getValue() + " " +t.getTag() + " " +t.getPriority());
+        }
+
+        Parser parser = new Parser(tokens);
+        if (parser.lang()){
+            System.out.println("\nDone");
+        }else{
+            System.out.println("\nError");
+        }
+
+        for (String p : parser.getPoliz()){
+            System.out.print(p + " ");
+        }
+
+        StackMachine stackMachine = new StackMachine(parser.getPoliz());
+        stackMachine.process();
+        stackMachine.print();
+    }
+}
